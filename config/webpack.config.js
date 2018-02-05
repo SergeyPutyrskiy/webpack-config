17373526bfe3d6283a3b48e9c2beef7f096e712c
sergp <sergeyputyrskiy@gmail.com>
2018-02-05 23:34:37 +0200

    Added config for html and css

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,9 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
     output: {
+        path: path.resolve(__dirname, '../dist'),        
         filename: 'app.js',
-        path: path.resolve(__dirname, '../dist')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader",
+                    publicPath: "/dist"
+                }),
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project',
+            template: path.resolve(__dirname, '../src/index.html'),
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true
+        }),
+        new ExtractTextPlugin({
+            filename: "styles.css",
+            disable: false,
+            allChunks: true
+        }),
+    ]
 };
